3c0020dcb11635d6c0ab987c8985e14bcb1b99fb
Sai Teja <Sai Teja@DESKTOP-N2VQBHK>
2021-06-23 16:48:20 +0530

    initial commit

---
+++ springboot2-springaop-example/src/main/java/net/guides/springboot2/aop/Springboot2SpringaopExampleApplication.java
@@ -0,0 +1,13 @@
+package net.guides.springboot2.aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2SpringaopExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot2SpringaopExampleApplication.class, args);
+	}
+
+}
---
+++ springboot2-springaop-example/src/test/java/net/guides/springboot2/aop/Springboot2SpringaopExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package net.guides.springboot2.aop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot2SpringaopExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
