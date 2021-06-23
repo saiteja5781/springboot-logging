22c51ae479342dcfb7c093c3dff93f36dbe1a8cd
Sai Teja <Sai Teja@DESKTOP-N2VQBHK>
2021-06-23 15:09:28 +0530

    initial commit

---
+++ springboot2-logging/src/main/java/net/guides/springboot2/logging/Springboot2LoggingApplication.java
@@ -0,0 +1,13 @@
+package net.guides.springboot2.logging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2LoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot2LoggingApplication.class, args);
+	}
+
+}
---
+++ springboot2-logging/src/test/java/net/guides/springboot2/logging/Springboot2LoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package net.guides.springboot2.logging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot2LoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
